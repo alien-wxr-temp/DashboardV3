5eaf12c197bd51d02a13f22dbd1d440e953e8419
alien-wxr-temp <49577496+alien-wxr-temp@users.noreply.github.com>
2019-08-30 17:27:08 +0800

    3.0.5

--- pyData/GetXML.py
+++ pyData/GetXML.py
@@ -4,16 +4,18 @@ from bs4 import BeautifulSoup
 import time
 import operator
 
+# Permalink from IT (Mars Wang)
 url = 'http://us-dal-cuic-p.natinst.com:8081/cuicui/permalink/?viewId=65B8C92C10000165005C5FD87D5BEAE0&linkType=xmlType&viewType=Grid'
 
-statelist = []
-timetick1 = int(time.time())
+# Declaration and Initialization
+statelist = []                      # A 2D-Array keeping the main state information of the phone support system 
+timetick1 = int(time.time())        # Keeping the previous timestamp of getting 
 timetick2 = int(time.time())
 with open('./pyData/nameList.txt', 'r') as f:
     for line in f:
         statelist.append(['',line.strip(),'','','','',0,0,0,0,0])
 
-inc = 27
+inc = 27                # waiting time
 
 allTalks = 0            #All Talks
 
@@ -54,9 +56,6 @@ while True:
 
         with open('./pyData/currentXML.txt', 'w') as f:
             try:
-                #f.writelines(time.strftime("%Y/%m/%d - %H:%M:%S", time.localtime())+'\n')
-                #f.writelines('|Numbers |AE Name           |newState |oTime |ifShift  \n')
-                #f.writelines('_'*60+'\n')
                 for row_tag in soup.report.children:
                     j=0
                     single = ['','','','','','','']
@@ -67,11 +66,7 @@ while True:
                             else:
                                 single[j] = 'NULL'
                             j=j+1
-                    #f.writelines('|'+single[0].ljust(8,' '))              #No.
-                    f.writelines(single[1])             #Name
-                    #f.writelines('|'+single[2].ljust(10,' '))              #State
-                    #f.writelines('|'+single[3].ljust(6,' '))              #TimeInState
-                    #f.writelines('|'+single[4].ljust(9,' '))              #ifOnShift
+                    f.writelines(single[1])                                 #Name
                     f.writelines('\n')
                     xml.append(single)
             except:
@@ -79,7 +74,6 @@ while True:
                 with open('./pyData/errorLog.txt','a') as f:
                     f.write(time.strftime("%Y/%m/%d - %H:%M:%S XML Exception", time.localtime())+'\n')
                     f.close()
-                time.sleep(inc/5)
                 continue
             f.close()
 
@@ -87,77 +81,89 @@ while True:
         xml2.sort(key=operator.itemgetter(1))
         i=0
         j=0
+        k=0
         while (i<len(statelist)):
-            while (j<len(xml2) and xml2[j][1]!=statelist[i][1]):
-                j=j+1
-            statelist[i][0]=xml2[j][0]
-            #OnShift or OffShift
-            if (xml2[j][4]=='true'):
-                xml2[j][4] = 'OnShift'
+            j=k
+            try:
+                while (xml2[j][1]!=statelist[i][1]):
+                    j=j+1
+                k=j+1
+            except:
+                print(statelist[i][1]+" is not on XML List")
+                with open('./pyData/errorLog.txt','a') as f:
+                    f.write(time.strftime("%Y/%m/%d - %H:%M:%S nameList Exception", time.localtime())+'\n')
+                    f.close()
+                i=i+1
+                continue
             else:
-                xml2[j][4] = 'OffShift'
-            #Talks Recording
-            if statelist[i][2]=='Ready' and xml2[j][2]=='Talking' and statelist[i][4]=='OnShift' and xml2[j][4]=='OnShift':
-                allTalks = allTalks+1
-                statelist[i][10] = statelist[i][10]+1
-            #Time Calculating
-            if xml2[j][3]=='NULL' or int(xml2[j][3])>=32768:                            # 18000s=5h, time overflow
-                xml2[j][3] = '32768'
-                statelist[i][2] = xml2[j][2]
-                statelist[i][3] = xml2[j][3]
-                statelist[i][6] = int(statelist[i][3])
-            else:
-                if xml2[j][2]=='NULL':                                                  # capture an intermediate state
-                    statelist[i][3] = xml2[j][3]
-                    statelist[i][6] = statelist[i][6]+timetick2-timetick1
-                elif statelist[i][4]=='OnShift' and statelist[i][2]!=xml2[j][2]:
-                    deltaTime = statelist[i][6]+timetick2-timetick1-int(xml2[j][3])
-                    if statelist[i][2]=='Ready':
-                        statelist[i][7] = deltaTime
-                        statelist[i][3] = xml2[j][3]
-                        statelist[i][6] = int(statelist[i][3])
-                    elif statelist[i][2]=='Talking':
-                        statelist[i][6] = deltaTime+int(xml2[j][3])
-                        statelist[i][3] = xml2[j][3]
-                    elif statelist[i][2]=='Work Ready':
-                        statelist[i][8] = deltaTime
-                        statelist[i][3] = xml2[j][3]
-                        statelist[i][6] = int(statelist[i][3])
-                    elif statelist[i][2]=='Not Ready':
-                        statelist[i][9] = deltaTime
-                        statelist[i][3] = xml2[j][3]
-                        statelist[i][6] = int(statelist[i][3])
-                    else:
-                        statelist[i][3] = xml2[j][3]
-                        statelist[i][6] = int(statelist[i][3])
-                    statelist[i][2] = xml2[j][2]
-                elif statelist[i][2]=='Work Ready':
-                    statelist[i][6] = statelist[i][6]+int(xml2[j][3])-int(statelist[i][3])
-                    statelist[i][3] = xml2[j][3]
+                statelist[i][0]=xml2[j][0]
+                #OnShift or OffShift
+                if (xml2[j][4]=='true'):
+                    xml2[j][4] = 'OnShift'
                 else:
+                    xml2[j][4] = 'OffShift'
+                #Talks Recording
+                if statelist[i][2]=='Ready' and xml2[j][2]=='Talking' and statelist[i][4]=='OnShift' and xml2[j][4]=='OnShift':
+                    allTalks = allTalks+1
+                    statelist[i][10] = statelist[i][10]+1
+                #Time Calculating
+                if xml2[j][3]=='NULL' or int(xml2[j][3])>=32768:                            # 18000s=5h, time overflow
+                    xml2[j][3] = '32768'
                     statelist[i][2] = xml2[j][2]
                     statelist[i][3] = xml2[j][3]
                     statelist[i][6] = int(statelist[i][3])
-            statelist[i][4] = xml2[j][4]
-            #State
-            if statelist[i][4]=='OnShift':
-                if statelist[i][2]=='Ready':
-                    statelist[i][5]='Free'
-                    freeNum = freeNum+1
-                    freeList.append(statelist[i][1])
-                elif statelist[i][2]=='Talking' or statelist[i][2]=='Work Ready':
-                    statelist[i][5]='Busy'
-                    busyNum = busyNum+1
-                    busyList.append(statelist[i][1])
-                elif statelist[i][2]=='Not Ready':
-                    statelist[i][5]='Away'
-                    awayNum = awayNum+1
-                    awayList.append(statelist[i][1])
                 else:
-                    statelist[i][5]='ErrState'
-            else:
-                statelist[i][5]='Offline'
-            i=i+1
+                    if xml2[j][2]=='NULL':                                                  # capture an intermediate state
+                        statelist[i][3] = xml2[j][3]
+                        statelist[i][6] = statelist[i][6]+timetick2-timetick1
+                    elif statelist[i][4]=='OnShift' and statelist[i][2]!=xml2[j][2]:
+                        deltaTime = statelist[i][6]+timetick2-timetick1-int(xml2[j][3])
+                        if statelist[i][2]=='Ready':
+                            statelist[i][7] = deltaTime
+                            statelist[i][3] = xml2[j][3]
+                            statelist[i][6] = int(statelist[i][3])
+                        elif statelist[i][2]=='Talking':
+                            statelist[i][6] = deltaTime+int(xml2[j][3])
+                            statelist[i][3] = xml2[j][3]
+                        elif statelist[i][2]=='Work Ready':
+                            statelist[i][8] = deltaTime
+                            statelist[i][3] = xml2[j][3]
+                            statelist[i][6] = int(statelist[i][3])
+                        elif statelist[i][2]=='Not Ready':
+                            statelist[i][9] = deltaTime
+                            statelist[i][3] = xml2[j][3]
+                            statelist[i][6] = int(statelist[i][3])
+                        else:
+                            statelist[i][3] = xml2[j][3]
+                            statelist[i][6] = int(statelist[i][3])
+                        statelist[i][2] = xml2[j][2]
+                    elif statelist[i][2]=='Work Ready':
+                        statelist[i][6] = statelist[i][6]+int(xml2[j][3])-int(statelist[i][3])
+                        statelist[i][3] = xml2[j][3]
+                    else:
+                        statelist[i][2] = xml2[j][2]
+                        statelist[i][3] = xml2[j][3]
+                        statelist[i][6] = int(statelist[i][3])
+                statelist[i][4] = xml2[j][4]
+                #State
+                if statelist[i][4]=='OnShift':
+                    if statelist[i][2]=='Ready':
+                        statelist[i][5]='Free'
+                        freeNum = freeNum+1
+                        freeList.append(statelist[i][1])
+                    elif statelist[i][2]=='Talking' or statelist[i][2]=='Work Ready':
+                        statelist[i][5]='Busy'
+                        busyNum = busyNum+1
+                        busyList.append(statelist[i][1])
+                    elif statelist[i][2]=='Not Ready':
+                        statelist[i][5]='Away'
+                        awayNum = awayNum+1
+                        awayList.append(statelist[i][1])
+                    else:
+                        statelist[i][5]='ErrState'
+                else:
+                    statelist[i][5]='Offline'
+                i=i+1
 
         with open('./pyData/currentState.txt', 'w') as f:
             f.writelines(time.strftime("%Y/%m/%d - %H:%M:%S", time.localtime())+'\n')
